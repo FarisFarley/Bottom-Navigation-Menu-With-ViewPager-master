d3e0949252e30c75b68b5738543acfe051f02a5a
Faris <farisfarley@hotmail.com>
2021-11-23 23:43:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bottom-Navigation-Menu-With-ViewPager
+Simple Bottom Navigation Menu With ViewPager. 
+
+![2019-10-23-03-55-00_screenRecording_1](https://user-images.githubusercontent.com/52538717/67327914-45911880-f54b-11e9-888c-f0748b0f8a5c.gif)
---
+++ app/src/main/java/com/wtsa/btm/MainActivity.java
@@ -0,0 +1,117 @@
+package com.wtsa.btm;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.wtsa.btm.ui.dashboard.DashboardFragment;
+import com.wtsa.btm.ui.home.HomeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    MenuItem prevMenuItem;
+    DashboardFragment dashBoardFragment;
+    HomeFragment homeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewPager viewPager = findViewById(R.id.viewerpager);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.navigation_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.navigation_dashboard:
+                        viewPager.setCurrentItem(1);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        dashBoardFragment = new DashboardFragment();
+        homeFragment = new HomeFragment();
+
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(dashBoardFragment);
+
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
+
+    }
+
+}
